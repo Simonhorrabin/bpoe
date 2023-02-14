1ea9bb38cb02ae296a8744018a74657bfc62279c
Simon Horrabin <simonhorrabin@gmail.com>
2023-02-13 23:57:27 -0500

    README

--- script.js
+++ script.js
@@ -9,6 +9,7 @@ function generatePassword() {
   var numericCharacters = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9'];
   var uppercaseCharacters = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z'];
   var lowercaseCharacters = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'];
+  var specialCharacters = ["!", "@", "#", "$", "%", "^", "*"]
   
   // Get the password criteria from the user
   var numberOfCharacters = prompt("How many characters do you want in your password? Choose between 8-128 characters");
