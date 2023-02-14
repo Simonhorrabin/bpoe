fc32467a49e5c8f9c63f269be867567937e1e380
Simon Horrabin <simonhorrabin@gmail.com>
2023-02-13 23:44:49 -0500

    corrected + finalized code

--- script.js
+++ script.js
@@ -1,66 +1,53 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
 
+// Function to generate the random password
 function generatePassword() {
-    console.log("Button clicked!");
-    var numericCharacters = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9'];
-    var uppercaseCharacters = [['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z'];]
-    var passwordLength =12;
-    var password = "";
+  console.log("Button clicked!");
 
-    // 1. Prompt the user for the password criteria
-   numberOfCharacters = prompt("How many charachters do you want in your Password? Choose between 8-128 charachters") 
-   if (numberOfCharacters < 8 || numberOfCharacters > 128) {
+  // Define the possible characters
+  var numericCharacters = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9'];
+  var uppercaseCharacters = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z'];
+  var lowercaseCharacters = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'];
+  
+  // Get the password criteria from the user
+  var numberOfCharacters = prompt("How many characters do you want in your password? Choose between 8-128 characters");
+  if (numberOfCharacters < 8 || numberOfCharacters > 128) {
     return "Choose the correct number of characters";
-   }
-   else if (isNaN(numberOfCharacters)){
+  } else if (isNaN(numberOfCharacters)) {
     numberOfCharacters = prompt("Please enter a valid number");
-   }
-   else {
-     alert("Your password will be" + numberOfCharacters + "charaters long.");
+  } else {
+    alert("Your password will be " + numberOfCharacters + " characters long.");
+  }
 
-   }
-   hasLowercase = confirm("Do you want lowercase characters?");
-   if (hasLowercase) {
-    var turnToLowercase = alert("Password has lowercase characters");
-   } 
-   else {
-    alert("Password will NOT have lowercase numericCharacters.");
-   }
-   hasUppercase = confirm("Do you want uppercase characters?");
-if (hasUppercase) {
-  alert("Password will have uppercase characters.");
-}
-else {
-  alert("Pass will Not have uppercase characters.");
+  var hasLowercase = confirm("Do you want lowercase characters?");
+  var hasUppercase = confirm("Do you want uppercase characters?");
+
+  // Build the list of possible characters based on user criteria
+  var possibleCharacters = numericCharacters;
+  if (hasLowercase) {
+    possibleCharacters = possibleCharacters.concat(uppercaseCharacters.map(char => char.toLowerCase()));
+  }
+  if (hasUppercase) {
+    possibleCharacters = possibleCharacters.concat(uppercaseCharacters);
+  }
+
+  // Generate the password
+  var finalPassword = "";
+  for (var i = 0; i < numberOfCharacters; i++) {
+    var rng = Math.floor(Math.random() * possibleCharacters.length);
+    finalPassword = finalPassword + possibleCharacters[rng];
+  }
+
+  return finalPassword;
 }
 
-    return "Generated password will go here!"
-}
 // Write password to the #password input
 function writePassword() {
   var password = generatePassword();
   var passwordText = document.querySelector("#password");
-
   passwordText.value = password;
-
 }
 
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
-
-for (var i = 0; i < enter; i++) {
-  var pickChoices = choices[Math.floor(Math.random() * choices.length)];
-  password.push(pickChoices);
-}
-// Enter text into the textbox
-function UserInput(newps) {
-  document.getElementById("password").textContent = newps;
-}
-// 
-const button = document.getElementById("myButton");
-
-button.addEventListener("click", function() {
-  // Code to execute when the button is clicked
-});
-console.log("button clicked!")
